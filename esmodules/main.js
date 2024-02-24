dfcfccd3a0de708c6304bdf7e13209a01dc628f5
Leo Jerez <contact.leojerez@gmail.com>
2024-02-24 01:18:50 -0500

    icon and basic structure working

--- esmodules/main.js
+++ esmodules/main.js
@@ -1,9 +1,41 @@
-console.log("Hello World! This code runs immediately when the file is loaded.");
+const moduleName = 'laugh-track';
+import { LaughTrack } from "./laughtrack.js"
+
+// export let i18n = key => {
+//     return game.i18n.localize(key);
+// };
+
+// export let setting = key => {
+//     return game.settings.get("breaktime", key);
+// };
 
 Hooks.on("init", function() {
-  console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+    // Keybinding
+    game.keybindings.register(moduleName, "playLaugh", {
+        name: 'Play Laugh',
+        hint: 'Plays the default laugh sound',
+        editable: [{ key: "KeyL", modifiers: []}],
+        onDown: () => {
+          window.game.laughTrack.play();
+        },
+        onUp: () => {},
+        restricted: false,  // Restrict this Keybinding to gamemaster only?
+        reservedModifiers: [],
+        precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL
+      });
+
+      let laughTrack = new LaughTrack();
+      window.game.laughTrack = laughTrack;
 });
 
-Hooks.on("ready", function() {
-  console.log("This code runs once core initialization is ready and game data is available.");
-});
+Hooks.on("getSceneControlButtons", function(controls) {
+    let tileControls = controls.find(x => x.name === "token");
+    tileControls.tools.push({
+      icon: "fa-solid fa-face-laugh-squint",
+      name: "send-a-laugh",
+      title: "Laugh Track",
+      button: true,
+      onClick: () => window.game.laughTrack.play()
+    });
+    
+  });
